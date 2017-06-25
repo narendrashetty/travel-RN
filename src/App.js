cdf84b8fb76c21f244e4aa46a5d4479262cc6594
Narendra N Shetty <mailnarendra15@gmail.com>
2017-06-25 13:57:10 +0200

    made the image drid screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
 } from 'react-native';
 
 import { StackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import ImageGrid from './ImageGrid';
 
@@ -17,9 +18,21 @@ export default StackNavigator({
     'backgroundColor': 'transparent',
   },
   'navigationOptions': {
+    'headerLeft': <Icon name="search" size={22} color="#fff" style={{marginLeft: 20,'textShadowColor': '#777','textShadowOffset': {'width': 1,'height': 1},'textShadowRadius': 0}} />,
+    'headerRight': <Icon name="bars" size={22} color="#fff" style={{marginRight: 20,'textShadowColor': '#777','textShadowOffset': {'width': 1,'height': 1},'textShadowRadius': 0}} />,
     'headerStyle': {
       'backgroundColor': 'transparent',
       'shadowOpacity': 0,
+      'paddingTop': 0,
+    },
+    'headerTitleStyle': {
+      'color': '#fff',
+      'textShadowColor': '#777',
+      'textShadowOffset': {
+        'width': 1,
+        'height': 1
+      },
+      'textShadowRadius': 0
     }
   }
 });
--- src/ImageGrid.js
+++ src/ImageGrid.js
@@ -3,24 +3,99 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Dimensions,
+  ScrollView
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
+const {height, width} = Dimensions.get('window');
+const padding = 20;
+
+const destinations = [{
+  'name': 'ICELAND',
+  'src': require('./images/iceland.jpg')
+}, {
+  'name': 'CANADA',
+  'src': require('./images/canada.jpg')
+}, {
+  'name': 'COLOMBIA',
+  'src': require('./images/colombia.jpg')
+}, {
+  'name': 'INDIA',
+  'src': require('./images/india.png')
+}, {
+  'name': 'FINLAND',
+  'src': require('./images/finland.jpg')
+}, {
+  'name': 'AMSTERDAM',
+  'src': require('./images/amsterdam.jpg')
+}, {
+  'name': 'LONDON',
+  'src': require('./images/london.jpg')
+}, {
+  'name': 'BARCELONA',
+  'src': require('./images/barcelona.jpg')
+}];
 
 export default class ImageGrid extends Component {
   static navigationOptions = {
-    'title': 'BEST IN TRAVEL',
-    'headerLeft': <Icon name="search" size={22} color="#000" style={{marginLeft: 12}} />,
-    'headerRight': <Icon name="bars" size={22} color="#000" style={{marginRight: 12}} />,
+    'title': 'BEST IN TRAVEL'
   };
 
+  renderImage(destination, i) {
+    return (
+      <View style={styles.imageContainer} key={i}>
+        <Image source={destination.src} style={styles.image} />
+        <Text style={styles.imageTitle}>{destination.name}</Text>
+      </View>
+    );
+  }
 
   render() {
-    return <View style={{backgroundColor: 'transparent'}}><Text>Hello World</Text></View>;
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          {destinations.map((des, i) => this.renderImage(des, i))}
+        </View>
+      </ScrollView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  
+  'container': {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  'imageContainer': {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    marginLeft: 20,
+    marginBottom: 20,
+    width: (width - padding * 3) / 2,
+    height: (width - padding * 3) / 2,
+  },
+
+  'image': {
+    width: (width - padding * 3) / 2,
+    height: (width - padding * 3) / 2,
+    borderRadius: 4
+  },
+
+  'imageTitle': {
+    position: 'absolute',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textShadowColor: '#3a3a3a',
+    textShadowOffset: {
+      width: 1,
+      height: 1
+    },
+    textShadowRadius: 0
+  }
 });
