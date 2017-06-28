622c1969079e96f28ceb2deb77551401a13d3f06
Narendra N Shetty <mailnarendra15@gmail.com>
2017-06-28 18:46:07 +0200

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -59,23 +59,6 @@ class TransitionerSwitcher extends Component {
             <Transitioner {...this.props} />
         );
     }
-    // For simplicity, we use context to pass these functions to PhotoGridScreen and SettingsScreen
-    // In real apps, we can use Redux to manage the state.
-    static childContextTypes = {
-        setActiveTransition: React.PropTypes.func,
-        getActiveTransition: React.PropTypes.func,
-    }
-    getChildContext() {
-        const self = this;
-        return {
-            setActiveTransition(transition:TransitionName) {
-                self.setState({ transition });
-            },
-            getActiveTransition():TransitionName {
-                return self.state.transition;
-            }
-        }
-    }
 }
 
 
